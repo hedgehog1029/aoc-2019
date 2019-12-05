08a806c34d30b1fed06ca0c20f01075ac1729b42
Henry Le Grys <henry@legrys.info>
2019-12-05 14:01:01 +0000

    Complete day 4

---
+++ day4-2.exs
@@ -0,0 +1,44 @@
+{:ok, input} = File.read("inputs/day4.txt")
+
+defmodule AOC do
+	def verify_increasing([digit | tail], biggest) do
+		if digit >= biggest do
+			verify_increasing(tail, digit)
+		else
+			false
+		end
+	end
+
+	def verify_increasing([], _biggest) do
+		true
+	end
+
+	def verify_double([digit | tail], last, beforethat) do
+		if digit == last and digit != List.first(tail) and digit != beforethat do
+			true
+		else
+			verify_double(tail, digit, last)
+		end
+	end
+
+	def verify_double([], _last, _beforethat) do
+		false
+	end
+
+	def apply_to(num) do
+		digits = num |> Integer.digits
+
+		verify_double(digits, 0, 0) and verify_increasing(digits, 0)
+	end
+
+	def run([start, finish]) do
+		range = Range.new(start, finish)
+
+		range |> Enum.count(&AOC.apply_to/1)
+	end
+end
+
+input |> String.trim |> String.split("-")
+|> Enum.map(&String.to_integer/1)
+|> AOC.run
+|> IO.puts
